366fd748ea85d3a577c831b4a89285fb96d1c1fd
Ken Johnson <dragonkenkj@gmail.com>
2021-02-04 21:50:44 -0500

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { loremIpsum } from "lorem-ipsum";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [numPs, setNumPs] = useState(0);
+  const [paragraphs, setParagraphs] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (numPs) {
+      const cParagraphs = [];
+      let i;
+      for (i = 0; i < numPs; i++) {
+        cParagraphs.push({
+          id: uuidv4(),
+          p: loremIpsum({ units: "paragraphs" }),
+        });
+      }
+      setParagraphs(cParagraphs);
+    } else {
+      console.log("no number enetered");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="title">Tired of boring lorem ipsum?</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="numPs">Paragraphs: </label>
+        <input
+          type="number"
+          id="numPs"
+          name="numPs"
+          value={numPs}
+          onChange={(e) => setNumPs(e.target.value)}
+        />
+        <button type="submit">Generate</button>
+      </form>
+      {paragraphs.map((paragraph) => {
+        const { id, p } = paragraph;
+        return <p key={id}>{p}</p>;
+      })}
     </div>
   );
-}
+};
 
 export default App;
